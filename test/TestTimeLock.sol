827e4262558b0c0a31179468dfa8aababaf425f2
Scott Chu <scottchu@icloud.com>
2018-04-26 16:21:51 -0400

    add time lock test and throw proxy for testing assert/require/rever errors

--- test/TestTimeLock.sol
+++ test/TestTimeLock.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.4.23;
 import "truffle/Assert.sol";
 import "../contracts/Gil.sol";
 import "../contracts/TimeLock.sol";
+import "./utils/ThrowProxy.sol";
 
 contract TestTimeLock {
   event Foo(uint x, uint y);
@@ -24,5 +25,23 @@ contract TestTimeLock {
       "beneficiary is set correctly");
     Assert.equal(lock.duration(), duration,
       "duration is set correctly");
+  }
+
+  function testReleaseBeforeReleaseTime() {
+    uint amount = 100;
+    Gil token = new Gil(amount);
+
+    address beneficiary = address(1);
+    uint duration = 100 days;
+
+    TimeLock lock = new TimeLock(token, beneficiary, duration);
+
+    ThrowProxy proxy = new ThrowProxy(address(lock));
+
+    TimeLock(address(proxy)).release();
+
+    bool output = proxy.execute.gas(200000)();
+
+    Assert.isFalse(output, "output is false as it throws error");
   }
 }
